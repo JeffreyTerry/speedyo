4017c056872cee13070af05d93b73fb2ef1e881c
Hong <hongj77@gmail.com>
2014-11-01 19:43:52 -0400

    working on scrollin

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,1 +1,23 @@
 var socket = io();
+
+function keyHandler(event) {
+  if (event.keyCode == 13) {
+    var value = $('#text_box').val();
+    $('#chat_box').append("<p class=\"user_one\">" + value +"</p>");
+    socket.emit('chat message', value);  
+
+    $('#text_box').val('');
+    $('#text_box').attr('placeholder', '');  
+
+    console.log($('#chat_box')[0].scrollHeight);
+    $('body').scrollTop($('#chat_box')[0].scrollHeight - $('#text_box').height());
+
+
+  }
+}
+
+
+
+$(document).ready(function () {
+  $('#text_box').keyup(keyHandler);
+});
--- config/routes.js
+++ config/routes.js
@@ -2,45 +2,44 @@ var _ = require('underscore'),
     request = require('request'),
     chat = require('../app/controllers/chat');
 
-module.exports = function(app, config){
-    app.get('/yo', function(req, res){
-        chat.findOpenChat(req.query, function(err, obj) {
-            if(err) {
-                if(req.query && req.query.username) {
-                    chat.createChat(req.query.username, function(err, object) {
-                        res.json({'response': 'OK'});
-                    });
-                }
-            } else {
+module.exports = function(app, config, io){
+  app.get('/yo', function(req, res){
+      chat.findOpenChat(req.query, function(err, obj) {
+          if(err) {
+              if(req.query && req.query.username) {
+                  chat.createChat(req.query.username, function(err, object) {
+                      res.json({'response': 'OK'});
+                  });
+              }
+          } else {
 
-                var formData1 = {'username': req.query.username,
-                                'api_token': '1d8ea23c-a1bf-4f18-b417-756e01d5b359',
-                                'link': 'http://74a4c901.ngrok.com/chat/' + obj._id};
-                var formData2 = {'username': obj.username,
-                                'api_token': '1d8ea23c-a1bf-4f18-b417-756e01d5b359',
-                                'link': 'http://74a4c901.ngrok.com/chat/' + obj._id};
-                request.post({url: 'https://api.justyo.co/yo/', form: formData1}, function(err, response1, body1) {
-                    request.post({url: 'https://api.justyo.co/yo/', form: formData2}, function(err, response2, body2) {
-                        if(err) {
-                            res.status(500).json({'err': err});
-                        } else {
-                            res.json(body2);
-                        }
-                    });
-                });
-            }
-        });
-        // var formData = {'to': 'THEONLYJEFF2',
-        //                 'username': 'THEONLYJEFF2',
-        //                 'link': 'http://google.com'};
-        // request.post({url: 'http://dev.justyo.co/rpc/yo_from_api_account', form: formData}, function(err, response, body) {
-        //     if(err) {
-        //         res.status(500).json({'err': err});
-        //     } else {
-        //         res.json(body);
-        //     }
-        // });
-    });
+              var formData1 = {'username': req.query.username,
+                              'api_token': '1d8ea23c-a1bf-4f18-b417-756e01d5b359',
+                              'link': 'http://74a4c901.ngrok.com/chat/' + obj._id};
+              var formData2 = {'username': obj.username,
+                              'api_token': '1d8ea23c-a1bf-4f18-b417-756e01d5b359',
+                              'link': 'http://74a4c901.ngrok.com/chat/' + obj._id};
+              request.post({url: 'https://api.justyo.co/yo/', form: formData1}, function(err, response1, body1) {
+                  request.post({url: 'https://api.justyo.co/yo/', form: formData2}, function(err, response2, body2) {
+                      if(err) {
+                          res.status(500).json({'err': err});
+                      } else {
+                          res.json(body2);
+                      }
+                  });
+              });
+          }
+      });
+      // var formData = {'to': 'THEONLYJEFF2',
+      //                 'username': 'THEONLYJEFF2',
+      //                 'link': 'http://google.com'};
+      // request.post({url: 'http://dev.justyo.co/rpc/yo_from_api_account', form: formData}, function(err, response, body) {
+      //     if(err) {
+      //         res.status(500).json({'err': err});
+      //     } else {
+      //         res.json(body);
+      //     }
+      // });
   });
 
   app.get('/fakeyo', function(req, res) {
@@ -52,11 +51,6 @@ module.exports = function(app, config){
   app.get('/', function(req,res) {
       res.render('home/home', {});
   });
-
-  io.on('connection', function (socket) {
-    console.log('hi');
-    socket.on('disconnect', function(){
-      console.log('bye');
 
   app.get('/chat/:cid', function(req, res) {
       chat.getCount(req.params.cid, function(err, count) {
@@ -80,7 +74,13 @@ module.exports = function(app, config){
       });
   });
 
+  io.on('connection', function (socket) {
+    socket.on('disconnect', function() {
+      console.log('bye');
+    });
   }); 
+  
+
 };
 
 
