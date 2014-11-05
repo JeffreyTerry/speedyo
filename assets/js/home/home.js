c141cdce1032a9d1daceb86125143d3a8165e744
JeffreyTerry <jterry94@gmail.com>
2014-11-05 11:13:47 -0600

    added waiting button

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -57,6 +57,7 @@ $(document).ready(function () {
   });
 
   socket.on('person entered', function() {
+    $('#clock').css('font-size', '2.5em');
     $('#clock').countdown(new Date().getTime() + 60 * 4 * 1000, function (event) {
       if (event.type == "finish") {
          $('body').append("<div class='faded'></div>");
