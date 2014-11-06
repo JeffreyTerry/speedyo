6391675349122a86edddd679e2fde3916481e9bd
JeffreyTerry <jterry94@gmail.com>
2014-11-05 21:29:49 -0600

    fixed bugs

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,15 +1,20 @@
 
 var socket = io(window.location.pathname.substring(5));
 
+function scrollToText() {
+  $('#chat_box').scrollTop( $('#chat_box')[0].scrollHeight);
+}
+
 function keyHandler(event) {
   //user presses enter
   if (event.keyCode == 13) {
 
     var value = $('#text_box').val();
+    if(value == '') return;
     if($('#chat_box').find('p').length > 0) {
       $('#chat_box').find('p').last().after("<p class=\"user_one wordwrap\">" + value.toUpperCase() +"</p>");
     } else {
-      $('#chat_box').prepend("<p class=\"user_one wordwrap\">" + value.toUpperCase() +"</p>");
+      $('#chat_box').append("<p class=\"user_one wordwrap\">" + value.toUpperCase() +"</p>");
     }
 
     socket.emit('chat message', value);  
@@ -17,7 +22,7 @@ function keyHandler(event) {
 
     $('#text_box').val('');
     $('#text_box').attr('placeholder', '');  
-    $('#chat_box').scrollTop( $('#chat_box')[0].scrollHeight);
+    scrollToText();
   } else {
     if($('#text_box').val().length == 0) {
       socket.emit('not typing');
@@ -32,7 +37,7 @@ $(document).ready(function () {
 
   socket.on('chat message', function (msg) {
     $('#chat_box').append("<p class=\"user_two wordwrap\">" + msg.toUpperCase() +"</p>");
-    $('#chat_box').scrollTop( $('#chat_box')[0].scrollHeight );
+    scrollToText();
     var blip = new Audio('../../sounds/blip.mp3');
     blip.play();
   });
@@ -40,12 +45,12 @@ $(document).ready(function () {
   socket.on('typing', function() {
     if ($('.typing').length == 0) {
       $('#chat_box').append("<div class='typing'><img height='29' src='../../imgs/typing.png'> </img></div>");
-      $('#chat_box').scrollTop( $('#chat_box')[0].scrollHeight);
+      scrollToText();
     }
   });
   socket.on('not typing', function() {
     $('.typing').remove();
-    $('#chat_box').scrollTop( $('#chat_box')[0].scrollHeight );  
+    scrollToText();
   });
 
   socket.on('left room', function() {
